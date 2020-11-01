d51d007d0e9d34c6bf67599bc03b57b3838d37b4
Madpeter <madpeter@gmail.com>
2020-11-01 02:37:44 +0000

    Update db.config.example.php

--- src/mysqli/db.config.example.php
+++ src/mysqli/db.config.example.php
@@ -1,12 +1,14 @@
 <?php
+
+namespace App;
+
 // this loads after db_objects but before mysqli loader!
 use YAPF\ErrorLogging as ErrorLogging;
 
-class db extends ErrorLogging
+class Db extends ErrorLogging
 {
-protected $dbHost = "localhost";
-protected $dbName = "";
-protected $dbUser = "";
-protected $dbPass = "";
+    protected $dbHost = "localhost";
+    protected $dbName = "";
+    protected $dbUser = "";
+    protected $dbPass = "";
 }
-?>
