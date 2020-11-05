a85c5dc7b1ccfe6df87284d97dce34e8c4a19565
Madpeter <madpeter@gmail.com>
2020-11-05 17:10:02 +0000

    testing if I dont overrite does it work
    :/

--- tests/gen/gen_Test.php
+++ tests/gen/gen_Test.php
@@ -17,7 +17,7 @@ class genTest extends TestCase
         define("GEN_DATABASE_USERNAME", "testsuser");
         define("GEN_DATABASE_PASSWORD", "testsuserPW");
         define("GEN_ADD_DB_TO_TABLE", false); // add the database name before the table name
-        define("GEN_SAVE_MODELS_TO", "src/Junk/");
+        define("GEN_SAVE_MODELS_TO", "tests/Junkoutput/");
         define("GEN_DATABASES", ["test"]);
         define("GEN_NAMESPACE", "YAPF\Junk");
         $this->sql = new MysqliConnector();
