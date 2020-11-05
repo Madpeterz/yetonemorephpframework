040b708e8ac38ad71c82fc07153f69d8a8584ae2
Madpeter <madpeter@gmail.com>
2020-11-05 16:09:04 +0000

    Update SqlConnectedClass.php
    makes getLastSql method open to all objects that extend the SQL connected class without having to call $sql directly

--- src/Core/SqlConnectedClass.php
+++ src/Core/SqlConnectedClass.php
@@ -10,7 +10,14 @@ abstract class SqlConnectedClass extends ErrorLogging
      * __construct
      * if not marked as disabled connects the sql global value
      */
-    protected function __construct()
+    public function getLastSql(): string
+    {
+        if ($this->sql != null) {
+            return $this->sql->getLastSql();
+        }
+        return "";
+    }
+    public function __construct()
     {
         global $sql;
         if ($this->disabled == false) {
