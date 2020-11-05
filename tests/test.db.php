ebbcc918785400c14cdd949af98d8701be1f813b
Madpeter <madpeter@gmail.com>
2020-11-05 09:04:55 +0000

    Update test.db.php

--- tests/test.db.php
+++ tests/test.db.php
@@ -7,7 +7,7 @@ use YAPF\Core\ErrorLogging as ErrorLogging;
 class Db extends ErrorLogging
 {
     protected $dbHost = "127.0.0.1";
-    protected $dbName = "test";
+    public $dbName = "test";
     protected $dbUser = "testsuser";
     protected $dbPass = "testsuserPW";
 }
