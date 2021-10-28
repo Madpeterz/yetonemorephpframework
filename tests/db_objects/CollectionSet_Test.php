9874ae4f8f5db56d7fd71c259e56bcb1cec0d5ff
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-28 11:06:15 +0100

    Removed REQUIRE_ID_ON_LOAD flag

--- tests/db_objects/CollectionSet_Test.php
+++ tests/db_objects/CollectionSet_Test.php
@@ -40,7 +40,6 @@ class CollectionSetTest extends TestCase
     protected function setUp(): void
     {
         global $sql;
-        define("REQUIRE_ID_ON_LOAD", true);
         $sql = new MysqliConnector();
     }
     protected function tearDown(): void
