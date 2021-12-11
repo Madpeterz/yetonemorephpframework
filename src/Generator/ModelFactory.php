3d38b7cfc38ceb2539ac6501a1d55c5ff537f045
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-12-11 16:48:51 +0000

    Update ModelFactory.php
    tweak spaces on gen

--- src/Generator/ModelFactory.php
+++ src/Generator/ModelFactory.php
@@ -227,11 +227,11 @@ class ModelFactory extends GeneratorWriter
                 $functionSetup = '(' . $use_type . ' $' . $row_two["COLUMN_NAME"] . '): ' . $returnType . '';
                 if ($enableLimits == true) {
                     $functionSetup = '(
-                    ' . $use_type . ' $' . $row_two["COLUMN_NAME"] . ', 
-                    int $limit = 0, 
-                    string $orderBy = "id", 
-                    string $orderDir = "DESC"
-    ): ' . $returnType;
+        ' . $use_type . ' $' . $row_two["COLUMN_NAME"] . ', 
+        int $limit = 0, 
+        string $orderBy = "id", 
+        string $orderDir = "DESC"
+        ): ' . $returnType;
                 }
                 $load_function .= $functionSetup;
                 if ($returnType == "array") {
