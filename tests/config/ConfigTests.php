02765b4cdbd75cf1deabed35e6e253a3feba0e7e
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-10 04:51:16 +0000

    Update ConfigTests.php

--- tests/config/ConfigTests.php
+++ tests/config/ConfigTests.php
@@ -3,18 +3,90 @@
 namespace YAPF\Junk;
 
 use PHPUnit\Framework\TestCase;
-use YAPF\Cache\Cache;
-use YAPF\Cache\Drivers\Redis;
 use YAPF\Config\SimpleConfig;
-use YAPF\Junk\Models\Counttoonehundo;
-use YAPF\Junk\Models\Liketests;
 use YAPF\Junk\Sets\CounttoonehundoSet;
-use YAPF\Junk\Sets\LiketestsSet;
 
 class ConfigTests extends TestCase
 {
-    public function test_yep(): void
+    public function test_forcePushToCache(): void
     {
-        $this->assertSame(true,true,"...");
+        global $system;
+        $system = new SimpleConfig();
+        $system->configCacheRedisTCP("127.0.0.1");
+        $system->setupCache();
+        $countto = new CounttoonehundoSet();
+        $system->getCacheDriver()->addTableToCache($countto->getTable(),15,true,true);
+        $system->startCache();
+        $countto = new CounttoonehundoSet();
+        $countto->loadAll();
+        $system->getCacheDriver()->shutdown();
+        
+        $system = new SimpleConfig();
+        $system->configCacheDisk("tmp");
+        $system->setupCache();
+        $countto = new CounttoonehundoSet();
+        $system->getCacheDriver()->addTableToCache($countto->getTable(),15,true,true);
+        $system->startCache();
+
+        $countto = new CounttoonehundoSet();
+        $countto->loadAll();
+        $system->getCacheDriver()->shutdown();
+
+        $this->assertSame(true,true,"yep");
+    }
+
+    /**
+     * @depends test_forcePushToCache
+     */
+    public function test_ConfigCacheFlagRedis(): void
+    {
+        global $system;
+        $system = new SimpleConfig();
+        $system->configCacheRedisTCP("127.0.0.1");
+        $system->setupCache();
+        $countto = new CounttoonehundoSet();
+        $system->getCacheDriver()->addTableToCache($countto->getTable(),15,true,true);
+        $system->startCache();
+
+        $countto = new CounttoonehundoSet();
+        $countto->loadAll();
+        $system->getCacheDriver()->shutdown();
+        $this->assertSame("Redis",$system->getCacheDriver()->getDriverName(),"Wrong cache driver");
+        $this->assertStringContainsString('"reads":1,',json_encode($system->getCacheDriver()->getStatusCounters()),"incorrect counters");
+        $this->assertSame(true,$system->getCacheDriver()->getStatusConnected(),"Redis did not connect");
+    }
+
+    /**
+     * @depends test_forcePushToCache
+     */
+    public function test_ConfigCacheFlagDisk(): void
+    {
+        global $system;
+        $system = new SimpleConfig();
+        $system->configCacheDisk("tmp");
+        $system->setupCache();
+        $countto = new CounttoonehundoSet();
+        $system->getCacheDriver()->addTableToCache($countto->getTable(),15,true,true);
+        $system->startCache();
+
+        $countto = new CounttoonehundoSet();
+        $countto->loadAll();
+        $system->getCacheDriver()->shutdown();
+        $this->assertSame("Disk",$system->getCacheDriver()->getDriverName(),"Wrong cache driver");
+        $this->assertStringContainsString('"reads":1,',json_encode($system->getCacheDriver()->getStatusCounters()),"incorrect counters");
+        $this->assertSame(true,$system->getCacheDriver()->getStatusConnected(),"Disk did not connect");
+    }
+
+    public function test_ConfigCacheFlagNoCache(): void
+    {
+        global $system;
+        $system = new SimpleConfig();
+        $countto = new CounttoonehundoSet();
+        $system->configCacheDisabled();
+        $system->setupCache();
+        $system->startCache();
+        $countto->loadAll();
+        $this->assertSame(null,$system->getCacheDriver(),"Wrong cache driver");
+        $this->assertSame(1,$system->getSQL()->getSQLstats()["selects"],"incorrect number of selects");
     }
 }
