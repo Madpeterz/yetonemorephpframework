0dcb86bab65b6599cf3dd85b0b5cc56ccb1fe82e
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-02-10 03:39:24 +0000

    remove junk

---
+++ tests/config/configTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace YAPF\Junk;
+
+use PHPUnit\Framework\TestCase;
+use YAPF\Cache\Cache;
+use YAPF\Cache\Drivers\Redis;
+use YAPF\Config\SimpleConfig;
+use YAPF\Junk\Models\Counttoonehundo;
+use YAPF\Junk\Models\Liketests;
+use YAPF\Junk\Sets\CounttoonehundoSet;
+use YAPF\Junk\Sets\LiketestsSet;
+
+class RedisCacheTests extends TestCase
+{
+    
+}
