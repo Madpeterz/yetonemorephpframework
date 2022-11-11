3519d04f30808751eebb995a75d8a8fe5848b7e8
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-11-11 22:22:54 +0000

    Update CacheWorker.php

--- src/Cache/CacheWorker.php
+++ src/Cache/CacheWorker.php
@@ -3,26 +3,17 @@
 namespace YAPF\Framework\Cache;
 
 use YAPF\Framework\Cache\Framework\CacheDriver;
-use YAPF\Framework\Responses\Cache\DeleteReply;
-use YAPF\Framework\Responses\Cache\ReadReply;
-use YAPF\Framework\Responses\Cache\WriteReply;
 
-class CacheWorker
+abstract class CacheWorker extends CacheLinkDriver
 {
     public function __construct(CacheDriver $driver)
     {
         $this->driver = $driver;
     }
-    protected string $keyPrefix = "";
-    protected ?CacheDriver $driver = null;
 
     public function __destruct()
     {
         $this->shutdown();
-    }
-    public function &getDriver(): CacheDriver
-    {
-        return $this->driver;
     }
 
     public function shutdown(): void
@@ -33,88 +24,72 @@ class CacheWorker
         $this->driver->stop();
     }
 
-    protected function deleteItem(string $key): DeleteReply
-    {
-        $keyPlusPrefix = $this->keyPrefix . $key;
+    public function getHash(
+        ?array $whereConfig = null,
+        ?array $orderConfig = null,
+        ?array $optionsConfig = null,
+        ?array $basicConfig = null,
+        string $table,
+        int $numberOfFields,
+        bool $asSingle = true
+    ): ?string {
         if ($this->haveDriver() == false) {
-            return new DeleteReply("No cache driver");
+            return null;
         }
-        if (array_key_exists($keyPlusPrefix, $this->pendingDeleteKeys) == true) {
-            return new DeleteReply("key already marked as deleted", true);
-        }
-        if (array_key_exists($keyPlusPrefix, $this->pendingWriteKeys) == true) {
-            unset($this->pendingWriteKeys[$keyPlusPrefix]);
-            unset($this->keys[$keyPlusPrefix]);
-        }
-        $this->pendingDeleteKeys[$keyPlusPrefix] = true;
-        return new DeleteReply("key marked to be removed", true);
-    }
-    protected function writeItem(string $key, string $value): WriteReply
-    {
-        $keyPlusPrefix = $this->keyPrefix . $key;
-        if ($this->haveDriver() == false) {
-            return new WriteReply("No cache driver");
-        }
-        if (array_key_exists($keyPlusPrefix, $this->pendingDeleteKeys) == true) {
-            unset($this->pendingDeleteKeys[$keyPlusPrefix]);
-        }
-        $this->pendingWriteKeys[$keyPlusPrefix] = true;
-        $this->loadKey($keyPlusPrefix, $value);
-        return new WriteReply("added to write Q, call save to write", true);
-    }
-    protected function getItem(string $key): ReadReply
-    {
-        $keyPlusPrefix = $this->keyPrefix . $key;
-        if ($this->haveDriver() == false) {
-            return new ReadReply("No cache driver");
-        }
-        if (array_key_exists($key, $this->pendingDeleteKeys) == true) {
-            return new ReadReply("key marked as deleted");
-        }
-        if ($this->seenKey($keyPlusPrefix) == true) {
-            return new ReadReply(
-                message:"from keys DB",
-                value:$this->keys[$keyPlusPrefix],
-                status:true
-            );
-        }
-        $reply = $this->driver->readKey($keyPlusPrefix);
-        if ($reply->status == false) {
-            return $reply;
-        }
-        $this->loadKey($keyPlusPrefix, $reply->value);
-        return new ReadReply("ok", $reply->value, true);
+        $raw = $this->giveJsonEncoded($whereConfig);
+        $raw .= $this->giveJsonEncoded($orderConfig);
+        $raw .= $this->giveJsonEncoded($optionsConfig);
+        $raw .= $this->giveJsonEncoded($basicConfig);
+        $raw .= json_encode(["table" => $table, "fieldscount" => $numberOfFields]);
+        return substr($this->sha256($raw . "cache"), 0, $this->driver->getKeyLength());
     }
 
-    protected function haveDriver(): bool
+    protected function giveJsonEncoded(?array $input): string
     {
-        if ($this->driver == null) {
+        if ($input === null) {
+            return "";
+        }
+        return json_encode($input);
+    }
+
+    public function cacheValid(string $table, string $hash, bool $asSingle = true): bool
+    {
+        if ($this->tableUsesCache($table, $asSingle) == false) {
             return false;
         }
-        return $this->driver->connected();
+        return $this->getItem($table . $hash)->status;
     }
 
-    // self store keys loaded in memory
-    protected array $keys = [];
-    protected array $pendingWriteKeys = [];
-    protected array $pendingDeleteKeys = [];
-
-    public function seenKey(string $key): bool
+    /**
+     * It reads a hash from the cache
+     * @param string table The name of the table you want to read from.
+     * @param string hash The hash to read from the table.
+     * @param bool asSingle If true, the table is a single row table. If false, the table is a multi-row
+     * table.
+     * @return ?mixed[] The data from the cache.
+     */
+    public function readHash(string $table, string $hash, bool $asSingle = true): ?array
     {
-        return array_key_exists($key, $this->keys);
-    }
-
-    public function loadKey(string $key, string $value): void
-    {
-        $this->keys[$key] = $value;
-    }
-
-    public function removeKey(string $key): bool
-    {
-        if ($this->seenKey($key) == false) {
-            return false;
+        if ($this->tableUsesCache($table, $asSingle) == false) {
+            return null;
         }
-        unset($this->keys[$key]);
-        return true;
+        $data = $this->getItem($table . $hash);
+        if ($data->status == false) {
+            return null;
+        }
+        return $this->tableUnpackString($table, $data->value);
+    }
+
+    public function writeHash(
+        string $table,
+        string $hash,
+        array $data,
+        bool $asSingle = true
+    ): bool {
+        if ($this->tableUsesCache($table, $asSingle) == false) {
+            return null;
+        }
+        $data = $this->writeItem($table . $hash, $this->tablePackString($table, $data));
+        return $data->status;
     }
 }
