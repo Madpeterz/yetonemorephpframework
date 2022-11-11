cdf796f2e0954684d9adfd2ea7447d4279d628e6
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-11-11 19:55:12 +0000

    cache framework

---
+++ src/Cache/Framework/CacheDriver.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace YAPF\Framework\Cache\Framework;
+
+use YAPF\Core\ErrorControl\ErrorLogging;
+use YAPF\Framework\Responses\Cache\CacheStatusReply;
+use YAPF\Framework\Responses\Cache\StatsReply;
+
+abstract class CacheDriver extends ErrorLogging implements CacheInterface
+{
+    protected bool $disconnected = true;
+    protected int $keyDeletes = 0;
+    protected int $keyWrites = 0;
+    protected int $keyReads = 0;
+
+    public function getStats(): StatsReply
+    {
+        return new StatsReply($this->keyReads, $this->keyWrites, $this->keyDeletes);
+    }
+
+    public function connected(): bool
+    {
+        return !$this->disconnected;
+    }
+
+    protected function readyToTakeAction(): bool
+    {
+        if ($this->disconnected == true) {
+            $this->addError("marked as disconnected");
+            return false;
+        }
+        return true;
+    }
+
+        /**
+     * It sets the connection settings to use a unix socket, and then starts the connection
+     * @return CacheStatusReply A CacheStatusReply object.
+     */
+    public function connectUnix(string $unixSocket): CacheStatusReply
+    {
+        return new CacheStatusReply("Unix socket Not supported for this driver");
+    }
+
+
+    /**
+     * It sets the connection settings to use TCP, and then starts the connection
+     * @return CacheStatusReply A CacheStatusReply object.
+     */
+    public function connectTCP(string $serverIP = "127.0.0.1", int $serverPort = 6379): CacheStatusReply
+    {
+        return new CacheStatusReply("TCP Not supported for this driver");
+    }
+
+    protected function __destruct()
+    {
+        $this->stop();
+    }
+}
---
+++ src/Cache/Framework/CacheInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace YAPF\Framework\Cache\Framework;
+
+use YAPF\Framework\Responses\Cache\CacheStatusReply;
+use YAPF\Framework\Responses\Cache\DeleteReply;
+use YAPF\Framework\Responses\Cache\ListKeysReply;
+use YAPF\Framework\Responses\Cache\PurgeReply;
+use YAPF\Framework\Responses\Cache\ReadReply;
+use YAPF\Framework\Responses\Cache\WriteReply;
+
+interface CacheInterface
+{
+    public function connected(): bool;
+    public function readKey(string $key): ReadReply;
+    public function writeKey(string $key, string $value, ?int $expireUnixtime = null): WriteReply;
+    public function deleteKeys(array $keys): PurgeReply;
+    public function deleteKey(string $key): DeleteReply;
+    public function listKeys(): ListKeysReply;
+    public function hasKey(string $key): CacheStatusReply;
+    public function purgeAllKeys(): PurgeReply;
+    public function start(): CacheStatusReply;
+    public function stop(): void;
+    public function driverName(): string;
+    public function setTimeout(int $timeout = 2): bool;
+}
