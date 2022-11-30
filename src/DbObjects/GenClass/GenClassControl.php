8513b693db5423f5dc58719e43ec1be2541f7796
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2022-11-30 18:09:53 +0000

    backport from 6.3.1

--- src/DbObjects/GenClass/GenClassControl.php
+++ src/DbObjects/GenClass/GenClassControl.php
@@ -44,4 +44,62 @@ abstract class GenClassControl extends GenClassFunctions implements Iterator
     }
 
     // end Iterator
+
+        /**
+     * It returns an array of fields that are the same as the default values
+     * @param checkFields An array of fields to check. If null, all fields will be checked.
+     * @param array excludeFields An array of fields to exclude from the list of fields to check.
+     * @return string[] a key value list of default fields and their values
+     */
+    public function getListDefaultFields(?array $checkFields = null, array $excludeFields = ["id"]): array
+    {
+        $class = get_class($this);
+        $copy = new $class();
+        $fields = $this->getFields();
+
+        $testFields = $fields;
+        if ($checkFields != null) {
+            $testFields = [];
+            foreach ($checkFields as $field) {
+                if (in_array($field, $fields) == false) {
+                    continue;
+                }
+                $testFields[] = $field;
+            }
+        }
+        $fields = [];
+        $fieldIsDefault = [];
+
+        foreach ($testFields as $field) {
+            if (in_array($field, $excludeFields) == true) {
+                continue;
+            }
+            $functionnameget = "get" . ucfirst($field);
+            if ($copy->$functionnameget() != $this->$functionnameget()) {
+                continue;
+            }
+            $fieldIsDefault[$field] = $this->$functionnameget();
+        }
+        return $fieldIsDefault;
+    }
+
+    /**
+     * It returns true if the value of the field is the same as the default value of the field
+     * if you are going to be checking multiple fields
+     * please use getListDefaultFields
+     */
+    public function isDefault(string $field): bool
+    {
+        $class = get_class($this);
+        $copy = new $class();
+        $fields = $this->getFields();
+        if (in_array($field, $fields) == false) {
+            return false;
+        }
+        $functionnameget = "get" . ucfirst($field);
+        if ($copy->$functionnameget() != $this->$functionnameget()) {
+            return false;
+        }
+        return true;
+    }
 }
