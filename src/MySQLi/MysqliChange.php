25ccde7524504f3662fae7e52c6ee47c99b2cfe6
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2023-01-02 00:37:58 +0000

    Update MysqliChange.php
    tweak

--- src/MySQLi/MysqliChange.php
+++ src/MySQLi/MysqliChange.php
@@ -153,7 +153,7 @@ abstract class MysqliChange extends MysqliWhere
             $index = $loop;
             $loop++;
             $updateConfig["values"][$index] = $this->convertIfBool($updateConfig["values"][$index]);
-            if (($updateConfig["values"][$index] === null) && ($updateConfig["values"][$index] !== 0)) {
+            if (($updateConfig["values"][$index] == null) && (intval($updateConfig["values"][$index]) !== 0)) {
                 $this->addError("switching field " . $updateConfig["fields"][$loop] .
                 " value to null " . $updateConfig["values"][$index]);
                 $sql .= "NULL";
